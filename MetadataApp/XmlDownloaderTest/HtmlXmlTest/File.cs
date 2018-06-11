19b02e786116f3cdff158698787216eb79bc2d70
Agnis <cocorifle@gmail.com>
2018-06-11 14:59:06 +0300

    Added constructors for File class

--- MetadataApp/XmlDownloaderTest/HtmlXmlTest/File.cs
+++ MetadataApp/XmlDownloaderTest/HtmlXmlTest/File.cs
@@ -2,10 +2,27 @@
 {
     internal class File
     {
+        public File(string name, string filename)
+        {
+            this.Set(name, filename, false);
+        }
+
+        public File(string name, string filename, bool attachment)
+        {
+            this.Set(name, filename, attachment);
+        }
+
         public string Name { get; set; }
 
         public string Filename { get; set; }
 
         public bool Attachment { get; set; }
+
+        private void Set(string name, string filename, bool attachment)
+        {
+            this.Name = name;
+            this.Filename = filename;
+            this.Attachment = attachment;
+        }
     }
 }

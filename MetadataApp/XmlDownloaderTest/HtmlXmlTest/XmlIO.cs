8f5e86a4da3e0ff23e6a1464e6646aa476d87d9c
Jeskadev <Jeskadev@gmail.com>
2018-06-11 14:47:06 +0300

    Exception impoval

--- MetadataApp/XmlDownloaderTest/HtmlXmlTest/XmlIO.cs
+++ MetadataApp/XmlDownloaderTest/HtmlXmlTest/XmlIO.cs
@@ -15,37 +15,44 @@
             }
         }
 
-        internal async Task SaveAsync(Task<XDocument> xmlDocument, string path, string fileName)
+        internal async Task<string> SaveAsync(Task<XmlData> xDocument, string path, string fileName)
         {
             this.CreateFolder(path);
-            //Console.WriteLine(path + fileName);
-            await Task.Run(() =>
+
+            // Console.WriteLine(path + fileName);
+            string errorMsg = await Task<string>.Run(() =>
             {
-                if (xmlDocument.Result != null)
+                if (xDocument.Result.Error == string.Empty && xDocument.Result.XDocument != null)
                 {
-                    xmlDocument.Result.Save(path + fileName);
+                    xDocument.Result.XDocument.Save(path + fileName);
                 }
+                else
+                {
+                    return xDocument.Result.Error;
+                }
+                return string.Empty;
             });
+            return errorMsg;
 
             // .NET Core 2.0 Has SaveAsync
         }
 
-        internal async Task<XDocument> LoadAsync(Task<string> stringDocument, LoadOptions loadOptions = LoadOptions.PreserveWhitespace)
+        internal async Task<XmlData> LoadAsync(Task<DownloadData> stringDocument, LoadOptions loadOptions = LoadOptions.PreserveWhitespace)
         {
-            return await Task.Run<XDocument>(() =>
+            return await Task.Run<XmlData>(() =>
             {
-                XDocument xDocument = null;
-                if (stringDocument.Result != null) {
-                    xDocument = XDocument.Parse(stringDocument.Result, loadOptions);
+                XmlData xmlData = new XmlData();
+                if (stringDocument.Result.Error == string.Empty && stringDocument.Result.ResponseString.Result != null)
+                {
+                    xmlData.XDocument = XDocument.Parse(stringDocument.Result.ResponseString.Result, loadOptions);
                 }
-                return xDocument;
+                else
+                {
+                    xmlData.Error = stringDocument.Result.Error;
+                }
+
+                return xmlData;
             });
-
         }
-
-
-
-
-
     }
 }

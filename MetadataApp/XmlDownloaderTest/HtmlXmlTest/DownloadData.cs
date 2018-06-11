289c8feb2b975860704946a2de46d62eeea303dc
Jeskadev <Jeskadev@gmail.com>
2018-06-11 14:50:48 +0300

    a
    a

---
+++ MetadataApp/XmlDownloaderTest/HtmlXmlTest/DownloadData.cs
@@ -0,0 +1,13 @@
+﻿namespace HtmlXmlTest
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
+
+    internal class DownloadData
+    {
+        internal Task<string> ResponseString { get; set; }
+
+        internal string Error { get; set; } = string.Empty;
+    }
+}
---
+++ MetadataApp/XmlDownloaderTest/HtmlXmlTest/XmlProcesser.cs
@@ -0,0 +1,125 @@
+﻿namespace HtmlXmlTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text.RegularExpressions;
+    using System.Xml;
+
+    internal class XmlProcesser
+    {
+        internal static List<Link> MainReader(string url, string localPath)
+        {
+            List<Link> links = new List<Link>();
+            WebResourceLoader webResourceLoader = new WebResourceLoader();
+            WebResponse myResponse = webResourceLoader.GetResponseFromSite(url).Result;
+            Stream myStream = myResponse.GetResponseStream();
+            XmlReader xmlReader = XmlReader.Create(myStream);
+
+            XmlWriter xmlWriter = XmlWriter.Create(localPath + "metadata.xml");
+            xmlWriter.WriteStartDocument();
+
+            xmlWriter.WriteStartElement("rest_api_metadata");
+            xmlWriter.WriteAttributeString("release", "36"); // EXAMPLE DATA
+            xmlWriter.WriteAttributeString("version", "510"); // EXAMPLE DATA
+
+            Console.WriteLine(localPath);
+
+            while (xmlReader.Read())
+            {
+                switch (xmlReader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        if (xmlReader.Name == "group")
+                        {
+                            string desc = string.Empty;
+                            while (xmlReader.NodeType != XmlNodeType.EndElement)
+                            {
+                                xmlReader.Read();
+                                if (xmlReader.NodeType == XmlNodeType.Text)
+                                {
+                                    desc = xmlReader.Value;
+
+                                    FixFilename(ref desc);
+                                }
+                            }
+
+                            localPath += desc + "\\";
+
+                            Directory.CreateDirectory(localPath);
+                            Console.WriteLine(localPath);
+
+                            xmlWriter.WriteStartElement("service_group");
+                            xmlWriter.WriteAttributeString("name", desc);
+                        }
+                        else if (xmlReader.Name == "link")
+                        {
+                            Link tempLink = new Link();
+                            while (xmlReader.NodeType != XmlNodeType.Text)
+                            {
+                                xmlReader.Read();
+                            }
+
+                            tempLink.Href = xmlReader.Value;
+
+                            xmlReader.Read();
+
+                            while (xmlReader.NodeType != XmlNodeType.Text)
+                            {
+                                xmlReader.Read();
+                            }
+
+                            tempLink.Description = xmlReader.Value;
+
+                            tempLink.Filepath = localPath;
+
+                            links.Add(tempLink);
+
+                            xmlWriter.WriteStartElement("service");
+                            xmlWriter.WriteAttributeString("description", tempLink.Description);
+                            xmlWriter.WriteAttributeString("href", tempLink.Href);
+                            xmlWriter.WriteEndElement();
+                        }
+
+                        // Console.WriteLine(xmlReader.Name + " EN");
+                        // Console.WriteLine(xmlReader.Value + " EV");
+                        break;
+                    case XmlNodeType.Text:
+                        // Console.WriteLine(xmlReader.Value + " TV");
+                        break;
+                    case XmlNodeType.EndElement:
+                        if (xmlReader.Name == "group")
+                        {
+                            var lastFolder = Path.GetDirectoryName(localPath);
+                            var pathWithoutLastFolder = Path.GetDirectoryName(lastFolder);
+                            localPath = pathWithoutLastFolder + "\\";
+
+                            xmlWriter.WriteEndElement();
+                        }
+
+                        // Console.WriteLine("END " + xmlReader.Name);
+                        break;
+                }
+            }
+
+            xmlReader.Close();
+            myResponse.Close();
+
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+
+            return links;
+        }
+
+        private static void FixFilename(ref string filename)
+        {
+            Regex regexSet = new Regex(@"([:*?\<>|])");
+            filename = filename.Replace("\\", "&");
+            filename = filename.Replace("/", "&");
+            filename = regexSet.Replace(filename, "$");
+            filename = filename.Replace(" ", "_");
+        }
+    }
+}

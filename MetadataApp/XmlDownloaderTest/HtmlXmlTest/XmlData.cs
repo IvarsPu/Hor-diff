70ff7f15f5ed53bf0ad0d734f751fbba376c9f3f
Jeskadev <Jeskadev@gmail.com>
2018-06-11 15:02:44 +0300

    Exception improvements

---
+++ MetadataApp/XmlDownloaderTest/HtmlXmlTest/XmlData.cs
@@ -0,0 +1,12 @@
+﻿namespace HtmlXmlTest
+{
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
+
+    internal class XmlData
+    {
+        internal XDocument XDocument { get; set; }
+
+        internal string Error { get; set; } = string.Empty;
+    }
+}

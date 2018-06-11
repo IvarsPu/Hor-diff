589d05ff8bf0206d9bc095943501a5bbe0ab29d3
Agnis <cocorifle@gmail.com>
2018-06-11 14:35:47 +0300

    New File class

---
+++ MetadataApp/XmlDownloaderTest/HtmlXmlTest/File.cs
@@ -0,0 +1,11 @@
+﻿namespace HtmlXmlTest
+{
+    internal class File
+    {
+        public string Name { get; set; }
+
+        public string Filename { get; set; }
+
+        public bool Attachment { get; set; }
+    }
+}
